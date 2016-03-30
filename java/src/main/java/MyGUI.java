b6e75c13660b247a2694003375f6e8de8354f611
ardenkim <kmk3170@uw.edu>
2016-03-30 10:52:28 -0700

    implement ActionListener

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -5,25 +5,40 @@ import java.util.*;
 /**
  * A demo of GUI programming with Swing
  */
-public class MyGUI extends JFrame {
+public class MyGUI extends JFrame implement ActionListener {
 	
 	public MyGUI() {
-		super("The Skywalker Family");
+		super("My GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton theButton = new JButton("Click me!");
 
+		// ActionListener listener = new ActionListener();	NO
+		ActionListener listener = this;
 
+		theButton.addActionListener(listener);
 
 
 		
-		this.add(theButton);
+		this.add(theButton);	// adding button
 		
-		this.pack();
-		this.setVisible(true);		
+		this.pack(); // optimize sizing
+		this.setVisible(true);		// please show up on the screen
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("You clicked me!");
+	}
+
 	public static void main(String[] args) {
     new MyGUI(); //create the frame
   }
 }
+
+/*
+	java.awt.*;
+		ActionListener will listen to objects and response for us
+		hears when an action occurs and shouts at us to do something
+		you can't say new ActionListener() because it's interface not class
+		--you have to implement ActionListener NOT new ActionListener()
+*/
